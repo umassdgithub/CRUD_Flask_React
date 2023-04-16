727b869013dd6c5ba39cf2e6228677f6a6718458
shashankhv <hvshashank@hotmail.com>
2023-04-16 15:42:17 -0400

    final commit

--- _frontend/users/src/App.jsx
+++ _frontend/users/src/App.jsx
@@ -1,23 +1,26 @@
-import React, { useState, useEffect,useRef } from 'react';
-import { Form, Button, Table } from 'react-bootstrap';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import { Form, Button, Table, Container, Row, Col } from "react-bootstrap";
+import Items from "./components/items";
+import axios from "axios";
+import "./App.css";
 
 const URL = "/api";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [editMode,setMode] = useState({mode:false,key:-1})
-  const addKey=useRef(null)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [editMode, setMode] = useState({ mode: false, key: -1 });
+  const addKey = useRef(null);
   // Fetch all users on component mount
   useEffect(() => {
-    axios.get(`${URL}/users`)
-      .then(response => {
+    axios
+      .get(`${URL}/users`)
+      .then((response) => {
         setUsers(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, []);
@@ -25,76 +28,99 @@ function App() {
   // Handle form submission for creating a new user
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post(`${URL}/users`, { name, email })
-      .then(response => {
+    axios
+      .post(`${URL}/users`, { name, email })
+      .then((response) => {
         setUsers([...users, response.data]);
-        setName('');
-        setEmail('');
+        setName("");
+        setEmail("");
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   };
   // Handle form submission for updating an existing user
   const handleUpdate = (user) => {
-    if(editMode.mode===false){
-      setMode({mode:true,key:user.id})
-      setName(user.name)
-      setEmail(user.email)
-      addKey.current.setAttribute("disabled", true)
-    }
-    else{
-      setMode({mode:false,key:-1})
-      addKey.current.removeAttribute("disabled")
-      axios.put(`${URL}/users/${user.id}`, { name, email })
-      .then(response => {
-        setUsers(users.map(u =>{
-            if(u.id===user.id){
-              u.name=name;
-              u.email=email;
-              return u
-            }
-            else{
-              return u
-            }
-        }))
-        setName('');
-        setEmail('');
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    if (editMode.mode === false) {
+      setMode({ mode: true, key: user.id });
+      setName(user.name);
+      setEmail(user.email);
+      addKey.current.setAttribute("disabled", true);
+    } else {
+      setMode({ mode: false, key: -1 });
+      addKey.current.removeAttribute("disabled");
+      axios
+        .put(`${URL}/users/${user.id}`, { name, email })
+        .then((response) => {
+          setUsers(
+            users.map((u) => {
+              if (u.id === user.id) {
+                u.name = name;
+                u.email = email;
+                return u;
+              } else {
+                return u;
+              }
+            })
+          );
+          setName("");
+          setEmail("");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
   };
 
   // Handle deletion of a user
   const handleDelete = (user) => {
-    axios.delete(`${URL}/users/${user.id}`)
-      .then(response => {
+    axios
+      .delete(`${URL}/users/${user.id}`)
+      .then((response) => {
         if (response.status === 200) {
-          setUsers(users.filter(u => u.id !== user.id));
+          setUsers(users.filter((u) => u.id !== user.id));
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   };
 
   return (
     <div className="App">
-      <h1>User Management</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h1 style={{ textAlign: "center" }}>User Management</h1>
+          </Col>
+        </Row>
+      </Container>
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicName">
+        <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter name" value={name} onChange={event => setName(event.target.value)} />
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter name"
+            readOnly={editMode.mode ? true : false}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value={email} onChange={event => setEmail(event.target.value)} />
+          <Form.Control
+            required
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            readOnly={editMode.mode ? true : false}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Form.Group>
 
-        <Button variant="primary" type="submit" ref={addKey} >
+        <Button variant="primary" type="submit" ref={addKey}>
           Add User
         </Button>
       </Form>
@@ -109,19 +135,18 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {
-            users.map(user => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>
-                <Button variant="primary" onClick={() => handleUpdate(user)}>
-                {editMode.key===user.id?<>OK</>:<>Edit</>}
-                </Button>{' '}
-                <Button variant="danger" onClick={() => handleDelete(user)}>Delete</Button>
-              </td>
-            </tr>
+          {users.map((user) => (
+            <Items
+              key={user.id}
+              user={user}
+              editmode={editMode}
+              name={name}
+              email={email}
+              setemail={setEmail}
+              setname={setName}
+              handledelete={handleDelete}
+              handleupdate={handleUpdate}
+            />
           ))}
         </tbody>
       </Table>
